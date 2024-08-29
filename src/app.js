34ea8faca7f4a3e0f38f5b71d651ad97d3429df3
Vadim <gvv.ghost@gmail.com>
2024-08-29 14:46:04 +0300

    Enabled token checking

--- src/app.js
+++ src/app.js
@@ -7,7 +7,12 @@ const express = require('express');
 const path = require('path');
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
+const jwtprivatekey = process.env.JWT_PRIVATE_KEY;
+if (!jwtprivatekey) {
+    throw new Error('JWT_PRIVATE_KEY is required for env variables');
+}
+const {verify} = require("jsonwebtoken");
 const app = express();
 
 app.set('views', path.join(__dirname, 'views'));
@@ -16,25 +21,31 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 
-// app.use((req, res, next) => {
-//     if (['/login', '/register'].includes(req.url)) {
-//         next();
-//     } else {
-//         const SESSION_EXPIRED = 429
-//         const token = req.headers.authorization;
-//         // console.log('token: ', token)
-//         try {
-//             const decoded = jwt.verify(
-//                 token.replace(/^Bearer\s+/, ""),
-//                 process.env.PRIVATE_KEY_FOR_JWT
-//             );
-//             if (decoded.exp > Date.now()) return res.status(SESSION_EXPIRED).send("Session expired") // there is no 492 code in HttpStatusCode
-//             next();
-//         } catch (err) {
-//             res.status(HttpStatusCode.Unauthorized).send();
-//         }
-//     }
-// });
+app.use((req, res, next) => {
+    if (['/login', '/register'].includes(req.url)) {
+        next();
+    } else {
+        const SESSION_EXPIRED = 429
+        const token = req.headers.authorization.replace(/^Bearer\s+/, "").trim();
+        if (token.length === 0) {
+            res.status(HttpStatusCode.Unauthorized).send("No token provided");
+            return;
+        }
+        const key = process.env.JWT_PRIVATE_KEY;
+        if (!key) {
+            throw new Error('JWT_PRIVATE_KEY is required');
+        }
+
+        try {
+
+            verify(token, key);
+            next();
+        } catch (err) {
+            // console.error('Session check error', err);
+            res.status(SESSION_EXPIRED).send();
+        }
+    }
+});
 
 mapRoutes(app);
 
